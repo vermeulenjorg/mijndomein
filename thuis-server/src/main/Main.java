5f8823b54ac7857194df290551a5907c9fbdcb82
Jorg Vermeulen <vermeulenjorg@gmail.com>
2018-12-02 16:45:34 +0100

    made communication with arduino

--- thuis-server/src/main/Main.java
+++ thuis-server/src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 import broker.Subscriber;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hoi");
-        final Subscriber subscriber = new Subscriber("testtopic/#",2,"tcp://broker.hivemq.com:1883");
-        subscriber.start();
+//        System.out.println("Hoi");
+//        final Subscriber subscriber = new Subscriber("testtopic/#",2,"tcp://broker.hivemq.com:1883");
+//        subscriber.start();
+
+
+
 
     }
 }
